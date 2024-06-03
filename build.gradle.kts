6edc47b65f915acf1267ad9d7689ff17dd998981
Dimas Rian Hastedi <83242712+dimrnhhh@users.noreply.github.com>
2024-06-04 02:09:26 +0700

    Add compose compiler gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.io.realm.kotlin) apply false
+    alias(libs.plugins.compose.compiler)
 }
