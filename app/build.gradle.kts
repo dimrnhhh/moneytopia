93c8a94d9195403c4105d137dfa6a65fa289a7fc
Dimas Rian Hastedi <83242712+dimrnhhh@users.noreply.github.com>
2024-07-25 23:15:56 +0700

    Remove unused library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     implementation(libs.patrykandpatrick.vico.compose)
     implementation(libs.patrykandpatrick.vico.compose.m3)
     implementation(libs.marosseleng.compose.material3.datetime.pickers)
-    implementation(libs.androidx.ui.text.android)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
