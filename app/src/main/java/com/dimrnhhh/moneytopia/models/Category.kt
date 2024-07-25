70d845b4f0350d9cb0be682fecf97516b43a1615
Dimas Rian Hastedi <83242712+dimrnhhh@users.noreply.github.com>
2024-07-25 21:05:37 +0700

    Add Category model

---
+++ app/src/main/java/com/dimrnhhh/moneytopia/models/Category.kt
@@ -0,0 +1,71 @@
+package com.dimrnhhh.moneytopia.models
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCard
+import androidx.compose.material.icons.outlined.Category
+import androidx.compose.material.icons.outlined.CreditCard
+import androidx.compose.material.icons.outlined.Event
+import androidx.compose.material.icons.outlined.FoodBank
+import androidx.compose.material.icons.outlined.House
+import androidx.compose.material.icons.outlined.LocalTaxi
+import androidx.compose.material.icons.outlined.MedicalServices
+import androidx.compose.material.icons.outlined.Payment
+import androidx.compose.material.icons.outlined.Payments
+import androidx.compose.material.icons.outlined.Savings
+import androidx.compose.material.icons.outlined.School
+import androidx.compose.material.icons.outlined.ShoppingBag
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import com.dimrnhhh.moneytopia.R
+
+sealed class Category {
+    data object Bills: Category()
+    data object Debt: Category()
+    data object Education: Category()
+    data object Family: Category()
+    data object FoodsAndDrinks: Category()
+    data object Healthcare: Category()
+    data object Savings: Category()
+    data object Shopping: Category()
+    data object SocialEvents: Category()
+    data object TopUp: Category()
+    data object Transportation: Category()
+    data object Others: Category()
+}
+
+@Composable
+fun Category.getName(): String {
+    return when (this) {
+        Category.Bills -> stringResource(R.string.bills)
+        Category.Debt -> stringResource(R.string.debt)
+        Category.Education -> stringResource(R.string.education)
+        Category.Family -> stringResource(R.string.family)
+        Category.FoodsAndDrinks -> stringResource(R.string.foods_and_drinks)
+        Category.Healthcare -> stringResource(R.string.healthcare)
+        Category.Savings -> stringResource(R.string.savings)
+        Category.Shopping -> stringResource(R.string.shopping)
+        Category.SocialEvents -> stringResource(R.string.social_events)
+        Category.TopUp -> stringResource(R.string.top_up)
+        Category.Transportation -> stringResource(R.string.transportation)
+        Category.Others -> stringResource(R.string.others)
+    }
+}
+
+@Composable
+fun Category.getIcon(): ImageVector {
+    return when (this) {
+        Category.Bills -> Icons.Outlined.Payments
+        Category.Debt -> Icons.Outlined.CreditCard
+        Category.Education -> Icons.Outlined.School
+        Category.Family -> Icons.Outlined.House
+        Category.FoodsAndDrinks -> Icons.Outlined.FoodBank
+        Category.Healthcare -> Icons.Outlined.MedicalServices
+        Category.Savings -> Icons.Outlined.Savings
+        Category.Shopping -> Icons.Outlined.ShoppingBag
+        Category.SocialEvents -> Icons.Outlined.Event
+        Category.TopUp -> Icons.Outlined.AddCard
+        Category.Transportation -> Icons.Outlined.LocalTaxi
+        Category.Others -> Icons.Outlined.Category
+    }
+}
