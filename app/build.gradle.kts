b54eb09ecbc0b72db5174b8ecb62b9e52fd0b3f0
Dimas Rian Hastedi <83242712+dimrnhhh@users.noreply.github.com>
2024-11-05 21:09:07 +0700

    Update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,14 +15,13 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
         ndk {
             abiFilters.add("arm64-v8a")
-            abiFilters.add("armeabi-v7a")
         }
     }
     buildTypes {
@@ -36,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
